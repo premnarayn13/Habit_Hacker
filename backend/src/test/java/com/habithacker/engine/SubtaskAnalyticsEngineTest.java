73b70a4f18c103711ee7f97ede69f65a7500804a
prem <naraynpremnl1304@gmail.com>
2026-08-22 23:56:00 +0530

    test(backend): add unit test template for SubtaskAnalyticsEngine

---
+++ backend/src/test/java/com/habithacker/engine/SubtaskAnalyticsEngineTest.java
@@ -0,0 +1,12 @@
+package com.habithacker.engine;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubtaskAnalyticsEngineTest {
+    @Test
+    void testSubtaskAnalytics() {
+        SubtaskAnalyticsEngine engine = new SubtaskAnalyticsEngine();
+        assertNotNull(engine);
+    }
+}
