beec4c91229c80f5a833be08a048de0332f88b7d
prem <naraynpremnl1304@gmail.com>
2026-08-22 23:55:48 +0530

    test(backend): add unit test template for CapacityEngine

---
+++ backend/src/test/java/com/habithacker/engine/CapacityEngineTest.java
@@ -0,0 +1,12 @@
+package com.habithacker.engine;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CapacityEngineTest {
+    @Test
+    void testCapacityCalculation() {
+        CapacityEngine engine = new CapacityEngine();
+        assertNotNull(engine);
+    }
+}
