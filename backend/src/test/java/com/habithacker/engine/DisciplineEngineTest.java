6e036b1ceaea8e608ead8dd091b33c2b4050dcce
prem <naraynpremnl1304@gmail.com>
2026-08-22 23:55:54 +0530

    test(backend): add unit test template for DisciplineEngine

---
+++ backend/src/test/java/com/habithacker/engine/DisciplineEngineTest.java
@@ -0,0 +1,12 @@
+package com.habithacker.engine;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DisciplineEngineTest {
+    @Test
+    void testDisciplineScoring() {
+        DisciplineEngine engine = new DisciplineEngine();
+        assertNotNull(engine);
+    }
+}
